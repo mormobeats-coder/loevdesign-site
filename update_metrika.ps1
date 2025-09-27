f85b3622fd10f6e513847cd984e3bf49aa5db895
Tinain <the.tinain@gmail.com>
2025-09-28 01:42:15 +0400

    Update Yandex.Metrika counter ID to 104304944

---
+++ update_metrika.ps1
@@ -0,0 +1,9 @@
+﻿ = @('404.html', 'blog-classification.html', 'blog-papa-brenda.html', 'blog-post.html', 'blog.html', 'case-rockot.html', 'case.html', 'page-template.html', 'privacy.html', 'showreel.html')
+foreach ( in ) {
+    if (Test-Path ) {
+        Write-Host "Updating ..."
+        (Get-Content ) -replace '104072618', '104304944' | Set-Content 
+        Write-Host "Done: "
+    }
+}
+Write-Host "All files updated!"
